74d4c4bd2945eabd30a6d1bea3e2da6ba45f428a
AnnieSoos <anniesoos007@gmail.com>
2018-03-10 16:56:18 +0100

    some action and css

--- 0hh1/src/App.js
+++ 0hh1/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Board from './containers/Board'
+import CreateGameButton from './components/CreateGameButton'
 
 
 class App extends Component {
@@ -10,6 +11,11 @@ class App extends Component {
       <div className="App">
         <h1 className="title">0hh1</h1>
         <Board />
+
+          <div className="actions">
+           <CreateGameButton />
+          </div>
+
       </div>
     )
   }
