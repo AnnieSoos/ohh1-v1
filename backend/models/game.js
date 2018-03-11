df24423bec9671cb5890c0bad9e2dd3588359073
AnnieSoos <anniesoos007@gmail.com>
2018-03-11 15:25:00 +0100

    datamodel fixd

--- backend/models/game.js
+++ backend/models/game.js
@@ -2,7 +2,9 @@
 module.exports = (sequelize, DataTypes) => {
   var Game = sequelize.define('Game', {
     gameId: DataTypes.INTEGER,
-    userId: DataTypes.INTEGER
+    userId: DataTypes.INTEGER,
+    board: DataTypes.STRING,
+    locked: DataTypes.STRING
   }, {});
   Game.associate = function(models) {
     // associations can be defined here
---
+++ backend/users/router.js
@@ -0,0 +1,66 @@
+const Router = require('express').Router
+const Users = require('./model')
+const bcrypt = require('bcrypt')
+const secret = require('../jwt').secret
+const sign = require('../jwt').sign
+
+const router = new Router()
+
+router.post('/users', (req, res) => {
+  const user = {
+    name: req.body.name,
+    password: bcrypt(req.body.password, 10)
+  }
+
+  Users
+  .create(user)
+    .then(entity => {
+      res.status(201)
+      res.json({
+        id: entity.id,
+        name: entity.name
+      })
+    })
+    .catch(err => {
+      res.status(422)
+      res.json({ message: err.message })
+    })
+  })
+
+  router.post('/login', (req, res) => {
+    const user = {
+      name: req.body.name,
+      password: bcrypt.hashSync(req.body.password, 10)
+    }
+
+    Users
+  	.findOne({
+  		where: {
+  			name: req.body.name
+  		}
+  	})
+  	.then(entity => {
+  		if (bcrypt.compareSync(req.body.password, entity.password)) {
+  			res.send({
+  				jwt: sign(entity.id),
+          message: "correct"
+  			})
+  		}
+  		else {
+  			res.status(400).send({
+  				message: 'Password was incorrect'
+  			})
+  		}
+  	})
+  	.catch(err => {
+  		console.error(err)
+  		res.status(500).send({
+  			message: 'Something went wrong'
+  		})
+  	})
+  })
+
+
+
+
+module.exports = router
