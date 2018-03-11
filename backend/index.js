e3a23bdfd7e7b355c12ea2ce71a3af20185359d0
AnnieSoos <anniesoos007@gmail.com>
2018-03-11 12:12:59 +0100

    setup backend

---
+++ backend/index.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const userRouter = require('./users/router')
+const verify = require('./jwt').verify
+const User = require('./users/model')
+
+const app = express()
+  .use(cors())
+  .use(bodyParser.json())
+
+
+app.listen(4001, () => console.log('Express API listening on port 4001'))
+
+
+app.listen(port, () => {
+  console.log(`
+  Server is listening on ${port}.
+  Open http://localhost:${port}.
+  to see the app in your browser.
+    `)
+})
+
+
+app.use(function (req, res, next) {
+  if (!req.headers.authorization) return next()
+
+  const auth = req.headers.authorization.split(' ')
+  if (auth[0] === 'Bearer') {
+    verify(auth[1], function (err, jwt) {
+      if (err) {
+        console.error(err)
+        res.status(400).send({
+          message: "JWT token invalid"
+        })
+      }
+      else {
+        User
+          .findById(jwt.id)
+          .then(entity => {
+            req.user = entity
+            next()
+          })
+          .catch(err => {
+            console.error(err)
+            res.status(500).send({
+              message: 'Something went wrong'
+            })
+          })
+      }
+    })
+  }
+  else next()
+})
+
+
+
+
+app.use(userRouter)
